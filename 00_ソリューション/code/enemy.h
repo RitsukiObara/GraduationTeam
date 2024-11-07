bb448cfc2abc76dffa79c86ca3289e12381ec260
TakayamaMomoya <119379245+TakayamaMomoya@users.noreply.github.com>
2024-11-07 11:56:22 +0900

    アザラシの小ターン実装

--- 00_ソリューション/code/enemy.h
+++ 00_ソリューション/code/enemy.h
@@ -92,6 +92,9 @@ public:
 	void EnableTurn(bool bValue) { m_bTurn = bValue; }	// 振り向きモーション
 	bool IsTurn(void) { return m_bTurn; }
 
+	void EnableMove(bool bValue) { m_bEnableMove = bValue; }	// 移動可能フラグ
+	bool IsEnableMove(void) { return m_bEnableMove; }
+
 	void SetState(E_State state) { m_state = state; }	// 状態
 	E_State GetState(void) { return m_state; }
 	void SetMove(D3DXVECTOR3 move) { m_move = move; }	// 移動量
@@ -109,6 +112,7 @@ private:
 	bool PathFind(int nIdxV, int nIdxH, vector<CIce*>& apIce);	// 探索の再帰関数
 	void MoveToNextGrid(void);	// 次のグリッドに向かって移動する
 	void JudgeTurn(void);	// 反転するかの判定
+	void JudgeCanMove(void);	// 移動できるかの判断
 	void CheckChangeGrid(void);	// グリッドが変わったかの確認
 	virtual void AliveDestGrid(void) {};	// 目的地に着いた時の仮想関数
 	void DriftDeath(void);	// 漂流中の死亡関数
@@ -122,6 +126,7 @@ private:
 	int m_nGridVDest;	// 目標のグリッドの縦番号
 	int m_nGridHDest;	// 目標のグリッドの横番号
 
+	bool m_bEnableMove;	// 移動可能フラグ
 	bool m_bTurn;	// 振り向きのフラグ
 	float m_fRotTurn;	// 振り向き角度
 	float m_fTimerDeath;	// 死亡までのタイマー
