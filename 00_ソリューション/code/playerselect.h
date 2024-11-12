47033c357622b1a9ec431cae494129c7077486b2
HorikawaShudai <221048@st.yoshida-g.ac.jp>
2024-11-12 10:59:12 +0900

    プレイヤー選択画面調整コミット

--- 00_ソリューション/code/playerselect.h
+++ 00_ソリューション/code/playerselect.h
@@ -24,6 +24,7 @@
 class CPlayerSelect;
 class CLight;
 class CUI;
+class CPlayer;
 
 //*****************************************************
 // クラスの定義
@@ -31,22 +32,7 @@ class CUI;
 class CPlayerSelect : public CScene
 {
 public:
-	enum E_State
-	{// 状態
-		STATE_NONE,		// 何もしていない状態
-		STATE_START,	// 開始状態
-		STATE_NORMAL,	// 通常状態 
-		STATE_RESULT,	// リザルト状態
-		STATE_END,		// 終了状態
-		STATE_MAX
-	};
-	enum E_GameMode
-	{// ゲームのモード
-		MODE_NONE = 0,	// 何でもないモード
-		MODE_SINGLE,	// シングルモード
-		MODE_MULTI,		// マルチ
-		MODE_MAX
-	};
+
 	enum E_StandyrState
 	{// プレイヤーの準備状態
 		
@@ -64,8 +50,6 @@ public:
 	virtual void Uninit(void);
 	virtual void Update();
 	virtual void Draw();
-	static void SetState(E_State state) { m_state = state; }
-	static E_State GetState(void) { return m_state; }
 
 	// 変数取得・設定関数
 	vector<CLight*> GetLight(void) { return m_aLight; };	// ライトの配列
@@ -86,14 +70,14 @@ private:
 	vector<CLight*> m_aLight;	// ゲーム中のライト
 	D3DXCOLOR m_colLight;	// ライト色
 	D3DXVECTOR3 m_posMid;	// プレイヤーと敵の中心座標
-	E_GameMode m_GameMode;	// ゲームモード
 	int m_nCountPlayer;
+
 	E_StandyrState m_StandbyState[MAX_PLAYER];	// プレイヤーの状態
 	CUI* m_apPlayerUI[MAX_PLAYER];	// プレイヤー用2Dオブジェクトのポインタ
 	CUI* m_apStateUI[MAX_PLAYER];	// プレイヤー用2Dオブジェクトのポインタ
+	CPlayer* m_apPlayer[MAX_PLAYER];// プレイヤーのポインタ
 
 	// 静的メンバ変数
-	static E_State m_state;	// 状態
 	static CPlayerSelect* m_pGame;	// 自身のポインタ
 
 };
