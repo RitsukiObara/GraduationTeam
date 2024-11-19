283fde87931f850f8d13cc9f83caf1be54015f0e
KazumaWakaki <221037@st.yoshida-g.ac.jp>
2024-11-19 15:54:02 +0900

    モーション託します

--- 00_ソリューション/code/selectmodepenguin.cpp
+++ 00_ソリューション/code/selectmodepenguin.cpp
@@ -34,6 +34,7 @@ CSelectModePenguin::CSelectModePenguin(int nPriority) : CMotion(nPriority)
 {
 	m_pCollisionSphere = nullptr;
 	m_move = D3DXVECTOR3(0.0f, 0.0f, 0.0f);
+	nCntMove = 0;
 }
 
 //=====================================================
@@ -70,6 +71,12 @@ CSelectModePenguin* CSelectModePenguin::Create(ISelectModePenguinState* pState)
 //=====================================================
 void CSelectModePenguin::SetState(ISelectModePenguinState* pState)
 {
+	if (m_pState != nullptr)
+	{
+		delete m_pState;
+		m_pState = nullptr;
+	}
+	
 	m_pState = pState;
 	if (m_pState != nullptr)
 	{
@@ -131,9 +138,6 @@ void CSelectModePenguin::Update(void)
 	// 移動量の減衰
 	m_move *= FACT_DECREASE_MOVE;
 
-	// モーションの管理
-	ManageMotion();
-
 	// モーション更新
 	CMotion::Update();
 
@@ -157,65 +161,33 @@ void CSelectModePenguin::Update(void)
 }
 
 //=====================================================
+// モーション状態
+//=====================================================
+void CSelectModePenguin::MotionState(void)
+{
+	nCntMove++;
+
+	if (nCntMove > 70)
+	{
+		int nRandNextMotion = universal::RandRange(CSelectModePenguin::MOTION::MOTION_WALK, CSelectModePenguin::MOTION::MOTION_WALK);
+
+
+		nCntMove = 0;
+	}
+
+	//else
+	//{
+	//	pPenguin->SetMotion(CSelectModePenguin::MOTION::MOTION_NEUTRAL);
+	//}
+}
+
+//=====================================================
 // 描画処理
 //=====================================================
 void CSelectModePenguin::Draw(void)
 {
 	// 継承クラスの描画
 	CMotion::Draw();
-}
-
-//=====================================================
-// ジャンプの終了
-//=====================================================
-void CSelectModePenguin::EndJump(void)
-{
-	// ジャンプモーションフラグを折る
-	m_fragMotion.bJump = false;
-
-	// 入力を有効化
-	EnableInput(true);
-}
-
-//=====================================================
-// モーションの管理
-//=====================================================
-void CSelectModePenguin::ManageMotion(void)
-{
-	int nMotion = GetMotion();
-	bool bFinish = IsFinish();
-
-	if (m_fragMotion.bJump)
-	{// ジャンプ中
-		if (nMotion == MOTION::MOTION_LANDING)
-		{// 着地モーション終了でジャンプ終了
-			if (bFinish)
-				EndJump();
-		}
-		else if (nMotion == MOTION::MOTION_STARTJUMP)
-		{// ジャンプ開始モーション終了からの遷移
-			if (bFinish)
-			{
-				SetMotion(MOTION::MOTION_STAYJUMP);
-			}
-		}
-		else if (nMotion != MOTION::MOTION_STARTJUMP)	// ジャンプ開始モーションの開始
-			SetMotion(MOTION::MOTION_STARTJUMP);
-	}
-	else if ((nMotion == MOTION_PECK || nMotion == MOTION_CANNOTPECK) && !bFinish)
-	{
-
-	}
-	else if (m_fragMotion.bWalk)
-	{// 歩きモーションフラグ有効
-		if (nMotion != MOTION::MOTION_WALK)
-			SetMotion(MOTION::MOTION_WALK);
-	}
-	else
-	{// 何もフラグが立っていない状態
-		if (nMotion != MOTION::MOTION_NEUTRAL)
-			SetMotion(MOTION::MOTION_NEUTRAL);
-	}
 }
 
 //*****************************************************
@@ -226,6 +198,8 @@ void CSelectModePenguin::ManageMotion(void)
 //=====================================================
 HRESULT CSelectModePenguinState_Stand::Init(CSelectModePenguin* pPenguin)
 {
+	pPenguin->SetMotion(CSelectModePenguin::MOTION::MOTION_NEUTRAL);
+
 	return S_OK;
 }
 
@@ -234,4 +208,131 @@ HRESULT CSelectModePenguinState_Stand::Init(CSelectModePenguin* pPenguin)
 //=====================================================
 void CSelectModePenguinState_Stand::Update(CSelectModePenguin* pPenguin)
 {
+
+}
+
+//*****************************************************
+// 移動ステート
+//*****************************************************
+//=====================================================
+// 初期化処理
+//=====================================================
+HRESULT CSelectModePenguinState_Move::Init(CSelectModePenguin* pPenguin)
+{
+	pPenguin->SetMotion(CSelectModePenguin::MOTION::MOTION_WALK);
+
+	return S_OK;
+}
+
+//=====================================================
+// 更新処理
+//=====================================================
+void CSelectModePenguinState_Move::Update(CSelectModePenguin* pPenguin)
+{
+
+}
+
+//*****************************************************
+// 転びステート
+//*****************************************************
+//=====================================================
+// 初期化処理
+//=====================================================
+HRESULT CSelectModePenguinState_Fall::Init(CSelectModePenguin* pPenguin)
+{
+	pPenguin->SetMotion(CSelectModePenguin::MOTION::MOTION_WALK);
+
+	return S_OK;
+}
+
+//=====================================================
+// 更新処理
+//=====================================================
+void CSelectModePenguinState_Fall::Update(CSelectModePenguin* pPenguin)
+{
+
+}
+
+//*****************************************************
+// 首振りステート
+//*****************************************************
+//=====================================================
+// 初期化処理
+//=====================================================
+HRESULT CSelectModePenguinState_ShakeHead::Init(CSelectModePenguin* pPenguin)
+{
+	pPenguin->SetMotion(CSelectModePenguin::MOTION::MOTION_WALK);
+
+	return S_OK;
+}
+
+//=====================================================
+// 更新処理
+//=====================================================
+void CSelectModePenguinState_ShakeHead::Update(CSelectModePenguin* pPenguin)
+{
+
+}
+
+//*****************************************************
+// 腹ベタステート
+//*****************************************************
+//=====================================================
+// 初期化処理
+//=====================================================
+HRESULT CSelectModePenguinState_Stomach::Init(CSelectModePenguin* pPenguin)
+{
+	pPenguin->SetMotion(CSelectModePenguin::MOTION::MOTION_WALK);
+
+	return S_OK;
+}
+
+//=====================================================
+// 更新処理
+//=====================================================
+void CSelectModePenguinState_Stomach::Update(CSelectModePenguin* pPenguin)
+{
+
+}
+
+//*****************************************************
+// 首うねステート
+//*****************************************************
+//=====================================================
+// 初期化処理
+//=====================================================
+HRESULT CSelectModePenguinState_UNYO::Init(CSelectModePenguin* pPenguin)
+{
+	pPenguin->SetMotion(CSelectModePenguin::MOTION::MOTION_WALK);
+
+	return S_OK;
+}
+
+//=====================================================
+// 更新処理
+//=====================================================
+void CSelectModePenguinState_UNYO::Update(CSelectModePenguin* pPenguin)
+{
+
+}
+
+//*****************************************************
+// 羽パタパタステート
+//*****************************************************
+//=====================================================
+// 初期化処理
+//=====================================================
+HRESULT CSelectModePenguinState_WingPTPT::Init(CSelectModePenguin* pPenguin)
+{
+	pPenguin->SetMotion(CSelectModePenguin::MOTION::MOTION_WALK);
+
+	return S_OK;
+}
+
+//=====================================================
+// 更新処理
+//=====================================================
+void CSelectModePenguinState_WingPTPT::Update(CSelectModePenguin* pPenguin)
+{
+
 }
