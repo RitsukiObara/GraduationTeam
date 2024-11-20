6b26fd676c03e28bd1f9606c930c4c271a3736c4
yuki hayakawa <221017@st.yoshida-g.ac.jp>
2024-11-20 14:20:26 +0900

    背景氷が四方向から出るように。 ゲーム画面全体に背景氷が来るようにした。 背景氷のモデルのサイズ修正

--- 00_ソリューション/code/ocean.cpp
+++ 00_ソリューション/code/ocean.cpp
@@ -33,15 +33,18 @@ namespace
 	const int OCEAN_ROT_CHANGE_TIME_DEGREE = 10;	// äCó¨å¸Ç´ïœçXéûä‘Ç‘ÇÍïù
 	const float FLOW_LEVEL_MULTIPLY = 0.008f;		// äCó¨ÇÃë¨ìxÇÃî{ó¶
 	const int MAX_ALBATROSS = 2;					// ÉAÉzÉEÉhÉäç≈ëÂêî
-	const int MAX_RANGE_LEFT = -2800;				// ÉâÉìÉ_ÉÄÉåÉìÉWç∂ç≈ëÂêî
-	const int MIN_RANGE_LEFT = -2000;				// ÉâÉìÉ_ÉÄÉåÉìÉWç∂ç≈í·êî
-	const int MAX_RANGE_RIGHT = 2000;				// ÉâÉìÉ_ÉÄÉåÉìÉWâEç≈ëÂêî
-	const int MIN_RANGE_RIGHT = 1200;				// ÉâÉìÉ_ÉÄÉåÉìÉWâEç≈í·êî
+	const int MAX_RANGE_LEFT = -1800;				// ÉâÉìÉ_ÉÄÉåÉìÉWç∂ç≈ëÂêî
+	const int MAX_RANGE_RIGHT = 1500;				// ÉâÉìÉ_ÉÄÉåÉìÉWâEç≈ëÂêî
+	const int MAX_RANGE_UP = 1200;					// ÉâÉìÉ_ÉÄÉåÉìÉWè„ç≈ëÂêî
+	const int MAX_RANGE_DOWN = -1500;				// ÉâÉìÉ_ÉÄÉåÉìÉWâ∫ç≈ëÂêî
 	const float Z_UP = 1500.0f;						// Zï˚å¸è„ï˚å¸
 	const float Z_DOWN = -1500.0f;					// Zï˚å¸â∫ï˚å¸
-	const int BGICE_CREATE_CNT_L = 250;				// îwåiïXÇ™ê∂ê¨Ç≥ÇÍÇÈïb(ç∂)
-	const int BGICE_CREATE_CNT_R = 300;				// îwåiïXÇ™ê∂ê¨Ç≥ÇÍÇÈïb(âE)
-
+	const float X_LEFT = -2500.0f;					// Xï˚å¸è„ï˚å¸
+	const float X_RIGHT = 2500.0f;					// Xï˚å¸â∫ï˚å¸
+	const int BGICE_CREATE_CNT_L = 200;				// îwåiïXÇ™ê∂ê¨Ç≥ÇÍÇÈïb(ç∂)
+	const int BGICE_CREATE_CNT_R = 200;				// îwåiïXÇ™ê∂ê¨Ç≥ÇÍÇÈïb(âE)
+	const int BGICE_CREATE_CNT_UP = 200;			// îwåiïXÇ™ê∂ê¨Ç≥ÇÍÇÈïb(è„)
+	const int BGICE_CREATE_CNT_DOWN = 200;			// îwåiïXÇ™ê∂ê¨Ç≥ÇÍÇÈïb(â∫)
 }
 
 //*****************************************************
@@ -61,8 +64,7 @@ COcean::COcean()
 	m_fRot = D3DXVECTOR3(0.0f, 0.0f, 0.0f);
 	m_nSetRotTime = 0;
 	m_nExecRotChangeTime = 0;
-	m_nBgiceCnt_L = 0;
-	m_nBgiceCnt_R = 0;
+	m_nBgiceCnt = 0;
 }
 
 //=====================================================
@@ -96,9 +98,8 @@ COcean* COcean::Create(void)
 //=====================================================
 HRESULT COcean::Init(void)
 {
-	m_nBgiceCnt_L = 0;
-	m_nBgiceCnt_R = 0;
-
+	m_nBgiceCnt = 0;
+	
 	CMeshField::Init();
 
 	CIceManager* pIceManager = CIceManager::GetInstance();
@@ -249,6 +250,18 @@ void COcean::BgIceRotState(void)
 		//è„Ç©ÇÁîwåiïXÇèoÇ∑èàóù
 		BgIceSetPosUp();
 	}
+
+	if (OceanFlowKeep == COcean::STREAM_LEFT)
+	{
+		//â∫Ç©ÇÁîwåiïXÇèoÇ∑èàóù
+		BgIceSetPosR();
+	}
+
+	if (OceanFlowKeep == COcean::STREAM_RIGHT)
+	{
+		//è„Ç©ÇÁîwåiïXÇèoÇ∑èàóù
+		BgIceSetPosL();
+	}
 }
 
 //====================================================
@@ -296,29 +309,17 @@ void COcean::OceanCycleTimer(void)
 //====================================================
 void COcean::BgIceSetPosUp(void)
 {
-	float posX_L = (float)universal::RandRange(MAX_RANGE_LEFT, MIN_RANGE_LEFT);
+	float posX = (float)universal::RandRange(MAX_RANGE_LEFT, MAX_RANGE_RIGHT);
 	CBgIce::TYPE type = (CBgIce::TYPE)universal::RandRange(CBgIce::TYPE_MAX, CBgIce::TYPE_BIG);
 
-	m_nBgiceCnt_L++;
-	m_nBgiceCnt_R++;
+	m_nBgiceCnt++;
 
-	if (m_nBgiceCnt_L >= BGICE_CREATE_CNT_L)
+	if (m_nBgiceCnt >= BGICE_CREATE_CNT_UP)
 	{
 		// îwåiïXÇÃÉçÅ[Éh
-		CBgIce::Create(D3DXVECTOR3(posX_L, 0.0f, Z_UP), D3DXVECTOR3(0.0f, 0.0f, 0.0f), type);
+		CBgIce::Create(D3DXVECTOR3(posX, 0.0f, Z_UP), D3DXVECTOR3(0.0f, 0.0f, 0.0f), type);
 
-		m_nBgiceCnt_L = 0;
-	}
-
-	float posX_R = (float)universal::RandRange(MAX_RANGE_RIGHT, MIN_RANGE_RIGHT);
-	type = (CBgIce::TYPE)universal::RandRange(CBgIce::TYPE_MAX, CBgIce::TYPE_BIG);
-
-	if (m_nBgiceCnt_R >= BGICE_CREATE_CNT_R)
-	{
-		// îwåiïXÇÃÉçÅ[Éh
-		CBgIce::Create(D3DXVECTOR3(posX_R, 0.0f, Z_UP), D3DXVECTOR3(0.0f, 0.0f, 0.0f), type);
-
-		m_nBgiceCnt_R = 0;
+		m_nBgiceCnt = 0;
 	}
 }
 
@@ -327,28 +328,55 @@ void COcean::BgIceSetPosUp(void)
 //====================================================
 void COcean::BgIceSetPosDown(void)
 {
-	float posX_L = (float)universal::RandRange(MAX_RANGE_LEFT, MIN_RANGE_LEFT);
+	float posX = (float)universal::RandRange(MAX_RANGE_LEFT, MAX_RANGE_RIGHT);
 	CBgIce::TYPE type = (CBgIce::TYPE)universal::RandRange(CBgIce::TYPE_MAX, CBgIce::TYPE_BIG);
 
-	m_nBgiceCnt_L++;
-	m_nBgiceCnt_R++;
+	m_nBgiceCnt++;
 
-	if (m_nBgiceCnt_L >= BGICE_CREATE_CNT_L)
+	if (m_nBgiceCnt >= BGICE_CREATE_CNT_DOWN)
 	{
 		// îwåiïXÇÃÉçÅ[Éh
-		CBgIce::Create(D3DXVECTOR3(posX_L, 0.0f, Z_DOWN), D3DXVECTOR3(0.0f, 0.0f, 0.0f), type);
+		CBgIce::Create(D3DXVECTOR3(posX, 0.0f, Z_DOWN), D3DXVECTOR3(0.0f, 0.0f, 0.0f), type);
 
-		m_nBgiceCnt_L = 0;
-	}
-
-	float posX_R = (float)universal::RandRange(MAX_RANGE_RIGHT, MIN_RANGE_RIGHT);
-	type = (CBgIce::TYPE)universal::RandRange(CBgIce::TYPE_MAX, CBgIce::TYPE_BIG);
-
-	if (m_nBgiceCnt_R >= BGICE_CREATE_CNT_R)
-	{
-		// îwåiïXÇÃÉçÅ[Éh
-		CBgIce::Create(D3DXVECTOR3(posX_R, 0.0f, Z_DOWN), D3DXVECTOR3(0.0f, 0.0f, 0.0f), type);
-
-		m_nBgiceCnt_R = 0;
+		m_nBgiceCnt = 0;
 	}
 }
+
+//====================================================
+// ç∂ï˚å¸Ç©ÇÁîwåiïXÇ™èoÇƒÇ≠ÇÈèàóù
+//====================================================
+void COcean::BgIceSetPosL(void)
+{
+	float posZ = (float)universal::RandRange(MAX_RANGE_UP, MAX_RANGE_DOWN);
+	CBgIce::TYPE type = (CBgIce::TYPE)universal::RandRange(CBgIce::TYPE_MAX, CBgIce::TYPE_BIG);
+
+	m_nBgiceCnt++;
+
+	if (m_nBgiceCnt >= BGICE_CREATE_CNT_L)
+	{
+		// îwåiïXÇÃÉçÅ[Éh
+		CBgIce::Create(D3DXVECTOR3(X_LEFT, 0.0f, posZ), D3DXVECTOR3(0.0f, 0.0f, 0.0f), type);
+
+		m_nBgiceCnt = 0;
+	}
+}
+
+//====================================================
+// âEï˚å¸Ç©ÇÁîwåiïXÇ™èoÇƒÇ≠ÇÈèàóù
+//====================================================
+void COcean::BgIceSetPosR(void)
+{
+	float posZ = (float)universal::RandRange(MAX_RANGE_UP, MAX_RANGE_DOWN);
+	CBgIce::TYPE type = (CBgIce::TYPE)universal::RandRange(CBgIce::TYPE_MAX, CBgIce::TYPE_BIG);
+
+	m_nBgiceCnt++;
+
+	if (m_nBgiceCnt >= BGICE_CREATE_CNT_R)
+	{
+		// îwåiïXÇÃÉçÅ[Éh
+		CBgIce::Create(D3DXVECTOR3(X_RIGHT, 0.0f, posZ), D3DXVECTOR3(0.0f, 0.0f, 0.0f), type);
+
+		m_nBgiceCnt = 0;
+	}
+}
+
